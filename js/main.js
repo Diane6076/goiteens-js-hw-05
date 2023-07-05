b8d1c4f9c998a8b126e383149d906582079c0ce9
diana <dskripka005@gmail.com>
2023-07-05 14:39:45 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// 1#
+const openModalBtn = document.querySelector('[data-action="open-modal]');
+const closeModalBtn = document.querySelector('[data-action="close-modal]');
+const backdrop = document.querySelector('backdrop');
+
+openModalBtn.addEventListener('click', () => {
+ openModalBtn.classList.toggle(show_modal);
+} );
+
+// 2#
+const color = document.querySelector('.color');
+
+color.addEventListener('color', () => {
+    document.body.style.backgroundColor = 'color';
+  });
+
+  // 3#
+
+  //4#
+
+
+  
+  // 5#
