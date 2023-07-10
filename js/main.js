ec8d30d6082f5b7db4c2dfa69c4b2ea8d6ba9de8
diana <dskripka005@gmail.com>
2023-07-10 20:22:24 +0300

    all

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
 // 1#
-const backdrop = document.querySelector('backdrop');
+const backdrop = document.querySelector(".backdrop");
 const openModalBtn = document.querySelector('[data-action="open-modal"]');
 const closeModalBtn = document.querySelector('[data-action="close-modal"]');
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-openModalBtn.addEventListener('click', () => {
- body.classList.add('show-modal');
+openModalBtn.addEventListener("click", () => {
+  body.classList.add("show-modal");
 });
 
-closeModalBtn.addEventListener('click', () => {
-    body.classList.remove('show-modal');
+closeModalBtn.addEventListener("click", () => {
+  body.classList.remove("show-modal");
 });
 
-backdrop.addEventListener('click', () => {
-    body.classList.remove('show-modal');
+backdrop.addEventListener("click", () => {
+  body.classList.remove("show-modal");
 });
-   
 
 // 2#
-const color = document.querySelector('.color');
-// const red = document.querySelector('.red');
-// const white = document.querySelector('.white');
-// const green = document.querySelector('.green');
+// const color = document.querySelector('.color');
+const red = document.querySelector(".red");
+const white = document.querySelector(".white");
+const green = document.querySelector(".green");
 
-color.addEventListener('color', () => {
-    document.body.style.backgroundColor = 'color';
+console.log(red);
+red.addEventListener("change", () => {
+  body.style.backgroundColor = "red";
 });
-// red.addEventListener('red', () => {
-//     document.body.style.backgroundColor = 'red';
-// });
-// white.addEventListener('white', () => {
-//     document.body.style.backgroundColor = 'pink';
-// });
-// green.addEventListener('green', () => {
-//     document.body.style.backgroundColor = 'yellow';
-// });
+white.addEventListener("change", () => {
+  document.body.style.backgroundColor = "white";
+});
+green.addEventListener("change", () => {
+  document.body.style.backgroundColor = "green";
+});
 
-  // 3#
+//4#
 
-  //4#
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
+input.addEventListener("input", () => {
+  if (input.value === "") {
+    output.textContent = "незнайомець";
+  } else {
+    output.textContent = input.value;
+  }
+});
 
-  
-  // 5#
+// 5#
+ const fontSizeControl = document.querySelector('#font-size-control')
+ const text = document.querySelector('#text')
+
+fontSizeControl.addEventListener('input',  handlSizeText);
+function  handlSizeText() {
+    text.style.fontSize = fontSizeControl.value+'px';
+    
+}
